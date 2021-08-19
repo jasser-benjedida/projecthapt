9b54d3e0f98f23f339b0232672faab800565b143
jasser-benjedida <benjedidajasser3@gmail.com>
2021-08-19 18:10:49 +0100

    edit page java

---
+++ hapt/src/main/java/com/horizon/hapt/TestController.java
@@ -0,0 +1,13 @@
+package com.horizon.hapt;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hapt")
+    public String horizon() {
+        return "notre projet";
+    }
+
+}
